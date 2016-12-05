d91221bf082a1ead9d3c6aa502cb02a9ceda07ba
Cabohydrina <catherine.dai@foxmail.com>
2016-12-06 00:57:28 +0800

    Salary first edition

---
+++ src/main/java/com/zhou/model/SalaryDetail.java
@@ -0,0 +1,42 @@
+package com.zhou.model;
+
+
+public class SalaryDetail {
+	private String eNo;
+	private Integer lno;
+	private Integer ono;
+	private Integer minus;
+	private Integer plus;
+	
+	public String geteNo() {
+		return eNo;
+	}
+	public void seteNo(String eNo) {
+		this.eNo = eNo;
+	}
+	public Integer getlno() {
+		return lno;
+	}
+	public void setlno(Integer lno) {
+		this.lno = lno;
+	}
+	public Integer getono() {
+		return ono;
+	}
+	public void setono(Integer ono) {
+		this.ono = ono;
+	}
+	public Integer getMinus() {
+		return minus;
+	}
+	public void setMinus(Integer minus) {
+		this.minus = minus;
+	}
+	public Integer getPlus() {
+		return plus;
+	}
+	public void setPlus(Integer plus) {
+		this.plus = plus;
+	}
+
+}
--- src/main/java/com/zhou/service/SalaryServiceImpl.java
+++ src/main/java/com/zhou/service/SalaryServiceImpl.java
@@ -1,5 +1,31 @@
 package com.zhou.service;
 
-public class SalaryServiceImpl {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.zhou.dao.SalaryDao;
+import com.zhou.model.Salary;
+import com.zhou.model.SalaryDetail;
+
+public class SalaryServiceImpl implements SalaryService{
+    @Autowired  
+    private SalaryDao SalaryDao; 
+
+	@Override
+	public List<Salary> GetSalaryList(String eNo) {
+		return SalaryDao.GetSalaryList(eNo);
+	}
+
+	@Override
+	public List<SalaryDetail> GetSalaryDetail(String eNo) {
+		return SalaryDao.GetSalaryDetail(eNo);
+	}
+
+	@Override
+	public int SetSalary(String eDepartment) {
+		Salary salary1=SalaryDao.ComputeSal(eDepartment);
+    	return SalaryDao.AddSal(salary1);
+	}
 
 }
